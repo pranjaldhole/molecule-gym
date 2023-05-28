e4819d1899f20152246051e27fa11ed1a96dfab5
Pranjal Dhole <dhole.pranjal@gmail.com>
2023-05-28 15:04:41 +0200

    bug fix configuration file

--- molecule_gym/config.py
+++ molecule_gym/config.py
@@ -21,7 +21,7 @@ RESULTS_DIR = osp.join(PROJECT_PATH, 'results')
 DATA_LIST = ['BBBP', 'clintox', 'sider', 'tox21', 'toxcast']
 
 def get_data_category(dataname):
-    if dataname in ['BBBP', 'clintox', 'sider', 'tox21', 'toxcast']
+    if dataname in ['BBBP', 'clintox', 'sider', 'tox21', 'toxcast']:
         data_category = 'physiology'
     else:
         raise AssertionError(f'{dataname} data does not have a category assigned!')
